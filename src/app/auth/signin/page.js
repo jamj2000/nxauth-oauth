ec5091119d965416d9982770c87d13afa79e9481
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2024-01-27 21:07:01 +0100

    ...

--- src/app/auth/signin/page.js
+++ src/app/auth/signin/page.js
@@ -2,6 +2,34 @@
 // han sido obtenidos de https://authjs.dev/img/providers/google.svg y otros proveedores
 import { loginGoogle, loginGithub } from "@/lib/actions"
 
+// // https://next-auth.js.org/configuration/pages#sign-in-page
+// const errors = {
+//   OAuthSignin: "Intenta iniciar sesión con una cuenta diferente.",
+//   OAuthCallback: "Intenta iniciar sesión con una cuenta diferente.",
+//   OAuthCreateAccount: "Intenta iniciar sesión con una cuenta diferente.",
+//   EmailCreateAccount: "Intenta iniciar sesión con una cuenta diferente.",
+//   Callback: "Intenta iniciar sesión con una cuenta diferente.",
+//   OAuthAccountNotLinked: "Para confirmar su identidad, inicie sesión con la misma cuenta que utilizó originalmente.",
+//   EmailSignin: "Comprueba tu dirección de correo electrónico.",
+//   CredentialsSignin: "Fallo al iniciar sesion. Verifique que los datos que proporcionó sean correctos.",
+//   SessionRequired: "Error al iniciar sesión. Verifique que los detalles que proporcionó sean correctos.",
+//   Default: "No se puede iniciar sesión.",
+// }
+
+// https://next-auth.js.org/configuration/pages#sign-in-page
+const errors = new Map();
+errors.set('OAuthSignin', "Intenta iniciar sesión con una cuenta diferente.");
+errors.set('OAuthCallback', "Intenta iniciar sesión con una cuenta diferente.");
+errors.set('OAuthCreateAccount', "Intenta iniciar sesión con una cuenta diferente.");
+errors.set('EmailCreateAccount', "Intenta iniciar sesión con una cuenta diferente.");
+errors.set('Callback', "Intenta iniciar sesión con una cuenta diferente.");
+errors.set('OAuthAccountNotLinked', "Este email ya está registrado con otro proveedor.");
+errors.set('EmailSignin', "Comprueba tu dirección de correo electrónico.");
+errors.set('CredentialsSignin', "Fallo al iniciar sesion. Verifique que los datos que proporcionó sean correctos.");
+errors.set('SessionRequired', "Error al iniciar sesión. Verifique que los detalles que proporcionó sean correctos.");
+errors.set('Default', "No se puede iniciar sesión.");
+
+
 function page({ searchParams }) {
 
   const { error } = searchParams
@@ -10,7 +38,7 @@ function page({ searchParams }) {
     <>
 
       <h1>Iniciar sesión</h1>
-      { error && <p>{error}</p>}
+      { error && <p>{errors.get(error)}</p>}
       <form>
 
         <button formAction={loginGoogle}>
