996fb4eaab49ede4c1037bd0fd81949f51e581ea
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2024-01-27 21:24:16 +0100

    ...

--- src/app/auth/signin/page.js
+++ src/app/auth/signin/page.js
@@ -2,27 +2,14 @@
 // han sido obtenidos de https://authjs.dev/img/providers/google.svg y otros proveedores
 import { loginGoogle, loginGithub } from "@/lib/actions"
 
-// // https://next-auth.js.org/configuration/pages#sign-in-page
-// const errors = {
-//   OAuthSignin: "Intenta iniciar sesión con una cuenta diferente.",
-//   OAuthCallback: "Intenta iniciar sesión con una cuenta diferente.",
-//   OAuthCreateAccount: "Intenta iniciar sesión con una cuenta diferente.",
-//   EmailCreateAccount: "Intenta iniciar sesión con una cuenta diferente.",
-//   Callback: "Intenta iniciar sesión con una cuenta diferente.",
-//   OAuthAccountNotLinked: "Para confirmar su identidad, inicie sesión con la misma cuenta que utilizó originalmente.",
-//   EmailSignin: "Comprueba tu dirección de correo electrónico.",
-//   CredentialsSignin: "Fallo al iniciar sesion. Verifique que los datos que proporcionó sean correctos.",
-//   SessionRequired: "Error al iniciar sesión. Verifique que los detalles que proporcionó sean correctos.",
-//   Default: "No se puede iniciar sesión.",
-// }
 
 // https://next-auth.js.org/configuration/pages#sign-in-page
 const errors = new Map();
-errors.set('OAuthSignin', "Intenta iniciar sesión con una cuenta diferente.");
-errors.set('OAuthCallback', "Intenta iniciar sesión con una cuenta diferente.");
-errors.set('OAuthCreateAccount', "Intenta iniciar sesión con una cuenta diferente.");
-errors.set('EmailCreateAccount', "Intenta iniciar sesión con una cuenta diferente.");
-errors.set('Callback', "Intenta iniciar sesión con una cuenta diferente.");
+errors.set('OAuthSignin', "Error al construir una URL de autorización.");
+errors.set('OAuthCallback', "Error al manejar la respuesta de un proveedor de OAuth.");
+errors.set('OAuthCreateAccount', "No se pudo crear un usuario proveedor de OAuth en la base de datos.");
+errors.set('EmailCreateAccount', "No se pudo crear un usuario de proveedor de correo electrónico en la base de datos.");
+errors.set('Callback', "Error en la ruta del controlador de devolución de llamada de OAuth.");
 errors.set('OAuthAccountNotLinked', "Este email ya está registrado con otro proveedor.");
 errors.set('EmailSignin', "Comprueba tu dirección de correo electrónico.");
 errors.set('CredentialsSignin', "Fallo al iniciar sesion. Verifique que los datos que proporcionó sean correctos.");
