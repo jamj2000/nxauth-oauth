eef31a3541276d2a1fcee22dc9c56c762493d3d9
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2025-02-17 22:26:46 +0100

    ...

--- src/auth.js
+++ src/auth.js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import Google from "@auth/core/providers/github"
 import GitHub from "@auth/core/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/lib/prisma"
+import prisma from "@/lib/prisma"
 
 const options = {
     providers: [Google, GitHub],
--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = globalThis.prisma || new PrismaClient()
+const prisma = global.prisma || new PrismaClient()
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+export default prisma
